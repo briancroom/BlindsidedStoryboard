b46be3bf3904efb62062d56a22a175fcdd82c9fb
Brian Croom <bcroom@pivotal.io>
2015-07-06 03:09:28 -0400

    Add nullability annotations

--- Pod/Classes/BlindsidedStoryboard+CrossStoryboardSegues.h
+++ Pod/Classes/BlindsidedStoryboard+CrossStoryboardSegues.h
@@ -1,5 +1,9 @@
 #import "BlindsidedStoryboard.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface BlindsidedStoryboard (CrossStoryboardSegues)
-- (instancetype)crossstoryboardsegues_storyboardWithName:(NSString *)name bundle:(NSBundle *)storyboardBundleOrNil;
+- (instancetype)crossstoryboardsegues_storyboardWithName:(NSString *)name bundle:(nullable NSBundle *)storyboardBundleOrNil;
 @end
+
+NS_ASSUME_NONNULL_END
--- Pod/Classes/BlindsidedStoryboard.h
+++ Pod/Classes/BlindsidedStoryboard.h
@@ -1,10 +1,14 @@
 #import <UIKit/UIKit.h>
 #import <Blindside/Blindside.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface BlindsidedStoryboard : UIStoryboard
 
 @property (nonatomic, strong) id<BSInjector> injector;
 
-+ (instancetype)storyboardWithName:(NSString *)name bundle:(NSBundle *)storyboardBundleOrNil injector:(id<BSInjector>)injector;
++ (instancetype)storyboardWithName:(NSString *)name bundle:(nullable NSBundle *)storyboardBundleOrNil injector:(id<BSInjector>)injector;
 
 @end
+
+NS_ASSUME_NONNULL_END
