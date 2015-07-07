756c9a8f8f4b77184da1d059411d1faeffda3247
Brian Croom <bcroom@pivotal.io>
2015-07-07 01:17:38 -0400

    Add support for Xcode 7 Storyboard References
    BlindsidedStoryboard can now propagate itself when moving between
    storyboards on iOS 9

--- Pod/Classes/BlindsidedStoryboard.m
+++ Pod/Classes/BlindsidedStoryboard.m
@@ -9,10 +9,16 @@
 }
 
 - (id)instantiateViewControllerWithIdentifier:(NSString *)identifier {
-    id controller = [super instantiateViewControllerWithIdentifier:identifier];
-    [self injectViewController:controller];
+    __block UIViewController *viewController;
+    [self performWithSwizzledFactoryMethod:^{
+        viewController = [super instantiateViewControllerWithIdentifier:identifier];
+    }];
 
-    return controller;
+    if (viewController.storyboard == self) {
+        [self injectViewController:viewController];
+    }
+
+    return viewController;
 }
 
 - (void)injectViewController:(UIViewController *)controller {
@@ -20,6 +26,36 @@
     for (id childController in [controller childViewControllers]) {
         [self injectViewController:childController];
     }
+}
+
+#pragma mark - Swizzling Helpers
+
+/// This method performs the given block in a special temporary environment such that any new storyboard
+/// that is created will be a BlindsidedStoryboard with the same injector as the receiver.
+- (void)performWithSwizzledFactoryMethod:(void (^)(void))actionsWhileSwizzled {
+    SEL factoryMethodSel = @selector(storyboardWithName:bundle:);
+    Method factoryMethod = class_getClassMethod([UIStoryboard class], factoryMethodSel);
+
+    __block id(*origImp)(id, SEL, id, id);
+    IMP newImp = imp_implementationWithBlock(^(id _self, NSString *name, NSBundle *bundle){
+
+        method_setImplementation(factoryMethod, (IMP)origImp);
+        origImp = NULL;
+
+        return [self blindsidedStoryboardWithName:name bundle:bundle];
+    });
+    origImp = (void *)method_setImplementation(factoryMethod, newImp);
+
+    actionsWhileSwizzled();
+
+    if (origImp) {
+        method_setImplementation(factoryMethod, (IMP)origImp);
+    }
+    imp_removeBlock(newImp);
+}
+
+- (instancetype)blindsidedStoryboardWithName:(NSString *)name bundle:(NSBundle *)storyboardBundleOrNil {
+    return [[self class] storyboardWithName:name bundle:storyboardBundleOrNil injector:self.injector];
 }
 
 @end
