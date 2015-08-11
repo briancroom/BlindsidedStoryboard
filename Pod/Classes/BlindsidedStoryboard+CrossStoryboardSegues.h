fdbb01beb8779d78e73e954f19e463c3940d415e
Brian Croom <bcroom@pivotal.io>
2015-08-10 23:21:50 -0400

    Add some inline documentation

--- Pod/Classes/BlindsidedStoryboard+CrossStoryboardSegues.h
+++ Pod/Classes/BlindsidedStoryboard+CrossStoryboardSegues.h
@@ -2,8 +2,14 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * This category enables integration with the CrossStoryboardSegues library (https://github.com/briancroom/CrossStoryboardSegues)
+ */
 @interface BlindsidedStoryboard (CrossStoryboardSegues)
+
+/// This method is used by the CrossStoryboardSegues library to allow injection to continue when switching storyboards.
 - (instancetype)crossstoryboardsegues_storyboardWithName:(NSString *)name bundle:(nullable NSBundle *)storyboardBundleOrNil;
+
 @end
 
 NS_ASSUME_NONNULL_END
